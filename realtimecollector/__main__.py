640a8696a534dd342fce140424056678235d7625
Conor Haining <conor.haining@gmail.com>
2017-11-17 20:14:56 +0000

    Scheudling Departure Scrapes
    Scrape the depature pages every minutes and
    tag them with the ATCOCode of the bus stop
    which is being scraped, once the jobs are initially
    queued then run the pending jobs forever.

--- realtimecollector/__main__.py
+++ realtimecollector/__main__.py
@@ -18,8 +18,23 @@ def main():
 
 	# Query for all Dundee Bus stops
 
-	# Scrape each bus stop
-	scraper.hello();
+	cursor = db.cursor()
+
+	cursor.execute('SELECT * FROM `bus_stops`')
+
+	for stop in cursor.fetchall():
+
+		ATCOCode = stop[1]
+
+		# Scrape every bus stop each minute
+		# Tag the job with the ATCOCode so that it can be easily terminated.
+		schedule.every(1).minutes.do(scraper.scrape, ATCOCode).tag(ATCOCode)
+
+
+	while 1:
+		schedule.run_pending()
+		pass
+
 	pass
 
 if __name__ == '__main__':
