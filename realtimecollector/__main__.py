03c2f1904ce962e632a2930523e860d30a056b90
Conor Haining <conor.haining@gmail.com>
2017-11-17 18:05:10 +0000

    Initial Commit

---
+++ realtimecollector/__main__.py
@@ -0,0 +1,16 @@
+import scraper
+import config
+
+
+def main():
+
+	# Establish Database Connection
+	
+	# Query for all Dundee Bus stops
+
+	# Scrape each bus stop
+	scraper.hello();
+	pass
+
+if __name__ == '__main__':
+	main()
---
+++ realtimecollector/config.py
@@ -0,0 +1,6 @@
+DATABASE_CONFIG = {
+	'host': 'host',
+	'user': 'usr',
+	'passwd': 'passwd',
+	'database': 'database',
+}
